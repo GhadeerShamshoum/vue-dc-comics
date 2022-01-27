865ddb3cd732939b0ae44f793ef772e7ff390833
Ghadeer <ghaora@gmail.com>
2022-01-27 19:09:44 +0200

    footer bottom+ almost done footer center

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,89 @@
 <template>
   <div>
-    <div class="footerTop">
-      <div class="container">
-        <div class="foterTopContainer" v-for='(element, index) in information' :key="index">
-          <img src="../assets/img/img_1.png" alt="">
-          <div class="text">{{element.text}}</div>
+    <footer>
+      <div class="footerTop">
+        <div class="container">
+          <div class="foterTopContainer" v-for='(element, index) in information' :key="index">
+            <img src="../assets/img/img_1.png" alt="">
+            <div class="text">{{element.text}}</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="footerCenter"></div>
-    <div class="footerBottom"></div>
-    
+      <div class="container">
+        <div class="footerCenter">
+          <div class="containerTextFooterC">
+            <div class="textContainerF">
+              <h1>dc comics</h1>
+              <div class="Text">
+                <div>Characters</div>
+                <div>Comics</div>
+                <div>TV</div>
+                <div>Games</div>
+                <div>Videos</div>
+                <div>News</div>
+                <div>shop</div>
+                <div>dc</div>
+                <div>site</div>
+              </div>
+            </div>
+             <div class="textContainerF">
+              <h1>dc comics</h1>
+              <div class="Text">
+                <div>Characters</div>
+                <div>Comics</div>
+                <div>TV</div>
+                <div>Games</div>
+                <div>Videos</div>
+                <div>News</div>
+                <div>Shop</div>
+                <div>DC</div>
+                <div>Site</div>
+              </div>
+            </div>
+             <div class="textContainerF">
+              <h1>shop</h1>
+              <div class="Text">
+                <div>Shop DC</div>
+                <div>Shop DC Collectibles</div>
+              </div>
+            </div>
+             <div class="textContainerF">
+              <h1>dc comics</h1>
+              <div class="Text">
+                <div>Characters</div>
+                <div>Comics</div>
+                <div>TV</div>
+                <div>Games</div>
+                <div>Videos</div>
+                <div>News</div>
+                <div>shop</div>
+                <div>dc</div>
+                <div>site</div>
+              </div>
+            </div>
+            
+          
+            
+
+          </div>
+          
+        </div>
+      </div>
+      <div>
+      <div class="footerBottom">
+        <div class="container B">
+          <button>sign-up now!</button>
+          <div class="bottomRight">follow us
+            <img class="icons" src="../assets/img/footer-facebook.png" alt="">
+            <img class="icons" src="../assets/img/footer-twitter.png" alt="">
+            <img class="icons" src="../assets/img/footer-youtube.png" alt="">
+            <img class="icons" src="../assets/img/footer-pinterest.png" alt="">
+            <img class="icons" src="../assets/img/footer-periscope.png" alt="">
+          </div>
+        </div>
+      </div>
+      </div>
+    </footer>   
   </div>
 </template>
 
@@ -40,6 +113,7 @@ export default {
           text: "dc power visa"
           
         }
+        
       ]
     }
   }
@@ -72,12 +146,82 @@ export default {
   }
 }
 
+.footerCenter{
+  height: 500px;
+  width: 100%;
+  background-image:url(../assets/img/dc-logo-bg.png);
+  background-position: right;
+  background-repeat: no-repeat;
+  .containerTextFooterC{
+    margin: 60px 0;
+    height: 80%;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    .textContainerF{
+      width: 220px;
+      .Text{
+        font-size: 15px;
+        padding: 10px;
+        color: gray;
+      }
+    }
+  }
+}
+
+.footerBottom{
+  height: 100px;
+  background-color: rgb(51, 50, 50);
+  color: $colorBlue;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-transform: uppercase;
+  button{
+    height: 50px;
+    width: 180px;
+    font-size: 20px;
+    text-transform: uppercase;
+    border: 2px solid $colorBlue;
+    background-color: transparent;
+    color: white;
+
+  }
+  .bottomRight{
+    line-height: 100px;
+    display: flex;
+    align-items: center;
+
+
+    .icons{
+      padding:0 10px;
+    }
+  }
+}
+
+.container.B{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+}
+
 .container{
   display: flex;
   justify-content: space-around;
   align-items: center;
 
+}
 
+footer{
+  background-image: url(../assets/img//footer-bg.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+h1{
+  text-transform: uppercase;
+  font-size: 25px;
 }
 
 </style>
