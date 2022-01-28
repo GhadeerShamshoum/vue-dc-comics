9b26e750942b6399570d67678b86b027be935650
Ghadeer <ghaora@gmail.com>
2022-01-28 17:37:03 +0200

    cards-main

---
+++ src/components/commons/SeriesCardsContainer.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="card">
+      <div class="containerImg">
+        <img :src="details.thumb" alt="">
+      </div>
+      <h5>{{details.series}}</h5>
+    </div> 
+</template>
+
+<script>
+export default {
+  name: 'SeriesCardsContainer',
+  props: {
+    details: Object,
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .card{
+    width: calc(100% / 6 - 20px);
+    margin: 50px 10px 0px 10px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    .containerImg{
+      width: 100%;
+      height: 70%;
+      overflow: hidden;
+
+      img{
+        width: 100%;
+      }
+    }
+  }
+
+
+</style>
--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -1,22 +1,34 @@
 <template>
   <main>
     <Jumbotron/>
+    <div class="container">
+    <SeriesCardsContainer
+    v-for="(element, index) in series"
+    :key="index"
+    :details="element"/>
+    </div>
      
   </main>
 </template>
 
 <script>
   import Jumbotron from '../commons/Jumbotron.vue'
-
+  import SeriesCardsContainer from '../commons/SeriesCardsContainer.vue'
+  import seriesdata from '../../data/Series.js'
+  
 export default {
   
   name: 'Main',
 
   components: { 
-    Jumbotron 
+    Jumbotron,
+    SeriesCardsContainer 
   },
 
   data() {
+    return {
+      series: seriesdata
+    }
     
   }
   
@@ -27,10 +39,20 @@ export default {
 <style scoped lang="scss">
 
 main{
-   height: 800px;
+   height: 1000px;
 } 
 
 h1{
   font-size: 20px;
 }
+
+.container{
+    margin: 0 auto;
+    width: 1100px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+
 </style>
